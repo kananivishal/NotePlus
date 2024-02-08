4715d3e7f771b4d32b33577ce0e34f7f16859fcf
Nikhil5858 <123rathodnikhil@gmail.com>
2024-02-08 14:20:31 +0530

    dashboard & login token set

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1901,54 +1901,18 @@ include "../NotePlus/includes/sidebar.php";
                         <h3 class="mb-3" id="modal-title"></h3>
                         <div class="btn-cancel p-0" data-dismiss="modal"><i class="las la-times"></i></div>
                     </div>
-                    <!-- <div class="content create-workform">
-                        <p class="mb-4" id="modal-body"></p>
+                    <div class="content create-workform">
+                    <p class="mb-4" id="modal-body" contenteditable="false"></p>
                         <h4 class="mb-3">Shared</h4>
                         <ul class="list-inline p-0 m-0">
                             <li>
                                 <div class="media align-items-center cust-card mb-3">
-                                    <div class="">
-                                        <img class="avatar-50 rounded-small" src="../assets/images/user/01.jpg" alt="01">
-                                    </div>
-                                    <div class="media-body ml-3">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <h5 class="mb-0">Anne Effit</h5>
-                                            <h6 class="mb-0">04 Hours Ago</h6>
-                                        </div>
-                                        <p class="mb-0">anneeffit@gmail.com</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="media align-items-center cust-card mb-3">
-                                    <div class="">
-                                        <img class="avatar-50 rounded-small" src="../assets/images/user/02.jpg" alt="01">
-                                    </div>
-                                    <div class="media-body ml-3">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <h5 class="mb-0">Poall Molve</h5>
-                                            <h6 class="mb-0">04 Hours Ago</h6>
-                                        </div>
-                                        <p class="mb-0">poallmolve@gmail.com</p>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="media align-items-center cust-card">
-                                    <div class="">
-                                        <img class="avatar-50 rounded-small" src="../assets/images/user/03.jpg" alt="01">
-                                    </div>
-                                    <div class="media-body ml-3">
-                                        <div class="d-flex align-items-center justify-content-between">
-                                            <h5 class="mb-0">Mack Devid</h5>
-                                            <h6 class="mb-0">05 Jan 2021</h6>
-                                        </div>
-                                        <p class="mb-0">mackdevid@gmail.com</p>
+                                    <div class="media-body ml-3" id="modal-collabrator">
                                     </div>
                                 </div>
                             </li>
                         </ul>
-                    </div> -->
+                    </div>
                 </div>
             </div>
         </div>
--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,11 @@
 <!doctype html>
 <html lang="en">
+
+<?php
+   if (isset($_COOKIE['TOKEN'])) {
+      header("Location: /noteplus/");
+  }
+?>
   <head>
     <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
