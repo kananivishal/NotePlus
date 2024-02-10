7b5895df5a30828d11ab43ab510dc0f02bf722fd
parmarkalpesh <parmarkalpesh1586@gmail.com>
2024-02-10 13:46:22 +0530

    sendmail&varify pages

---
+++ pages/verified.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>NotePlus</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/noteplus/assets/images/favicon.ico" />
+    <link rel="stylesheet" href="/noteplus/assets/css/backend-plugin.min.css?v=1.0.0">
+    <link rel="stylesheet" href="/noteplus/assets/css/backend.css?v=1.0.0">
+    <link rel="stylesheet" href="/noteplus/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/noteplus/assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
+    <link rel="stylesheet" href="/noteplus/assets/vendor/remixicon/fonts/remixicon.css">
+    <link rel="stylesheet" href="/noteplus/assets/vendor/@icon/dripicons/dripicons.css">
+
+    <script src="/noteplus/js/init.js"></script>
+
+</head>
+
+<body class="">
+    <!-- loader Start -->
+    <div id="loading">
+        <div id="loading-center">
+        </div>
+    </div>
+    <!-- loader END -->
+    <div class="wrapper">
+        <section class="login-content">
+            <div class="container h-100">
+                <div class="row justify-content-center align-items-center height-self-center">
+                    <div class="col-md-5 col-sm-12 col-12 align-self-center">
+                        <div class="sign-user_card" id="signup-form">
+                            <div class="logo-detail">
+                                <div class="d-flex align-items-center">
+                                    <img src="/noteplus/assets/images/logo.png" class="img-fluid rounded-normal light-logo logo" alt="logo">
+                                    <h4 class="logo-title ml-3">NotePlus</h4>
+                                </div>
+                            </div>
+                            <h3 class="mb-2">Your Email is Successfully Verified.</h3>
+                            <!-- Email Form -->
+                            <form id="email-form">
+                            <a href="/noteplus/pages/login.php" type="button" id="btnemail" class="btn btn-primary" >Login</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <script src="/noteplus/js/signup.js"></script>
+
+    <script src="/noteplus/assets/js/backend-bundle.min.js"></script>
+    <script src="/noteplus/assets/js/flex-tree.min.js"></script>
+    <script src="/noteplus/assets/js/tree.js"></script>
+    <script src="/noteplus/assets/js/table-treeview.js"></script>
+    <script src="/noteplus/assets/js/sweetalert.js"></script>
+    <script src="/noteplus/assets/js/chart-custom.js"></script>
+    <script src="/noteplus/assets/js/slider.js"></script>
+    <script src="/noteplus/assets/js/app.js"></script>
+
+</body>
+
+</html>
